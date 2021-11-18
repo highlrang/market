cf302ab7ff35ec81948a21b8724371e5d6a80d35
jhw127 <jhw127@naver.com>
2021-11-18 23:27:48 +0900

    orderController integration test 2

--- web/src/test/java/com/myproject/myweb/controller/order/OrderControllerIntegrationTest.java
+++ web/src/test/java/com/myproject/myweb/controller/order/OrderControllerIntegrationTest.java
@@ -1,54 +1,70 @@
 package com.myproject.myweb.controller.order;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myproject.myweb.controller.OrderController;
-import com.myproject.myweb.domain.Order;
+import com.myproject.myweb.domain.*;
 import com.myproject.myweb.domain.user.Customer;
+import com.myproject.myweb.domain.user.Seller;
 import com.myproject.myweb.dto.order.OrderResponseDto;
 import com.myproject.myweb.dto.user.CustomerResponseDto;
 import com.myproject.myweb.repository.CustomerRepository;
+import com.myproject.myweb.repository.ItemRepository;
 import com.myproject.myweb.repository.OrderRepository;
+import com.myproject.myweb.repository.SellerRepository;
 import com.myproject.myweb.service.CartService;
 import com.myproject.myweb.service.ItemService;
 import com.myproject.myweb.service.OrderService;
 import com.myproject.myweb.service.PaymentService;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import org.hamcrest.Matchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-// @EnableAutoConfiguration
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= {OrderController.class,
-                          OrderService.class,
-                          OrderRepository.class,
-                          CustomerRepository.class})
+@SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class OrderControllerIntegrationTest {
+    @Autowired private SellerRepository sellerRepository;
     @Autowired private CustomerRepository customerRepository;
+    @Autowired private ItemRepository itemRepository;
     @Autowired private OrderRepository orderRepository;
     @Autowired private OrderService orderService;
     @Autowired private OrderController orderController;
@@ -62,42 +78,79 @@ public class OrderControllerIntegrationTest {
 
     @Test
     public void detailview() throws Exception{
-        List<Order> orderList = orderRepository.findAll();
+        Customer customer = customerRepository.save(Customer.builder()
+                .name("test customer")
+                .email("test email")
+                .password("test pw")
+                .build());
+        Seller seller = sellerRepository.save(Seller.builder()
+                .name("test seller")
+                .email("test email")
+                .password("test pw")
+                .build()
+        );
+        Item item = itemRepository.save(
+                Item.createItem(Category.CLOTHES, seller, "test clothes", 10000, 500)
+        );
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), 1);
+        Order order = orderRepository.save(Order.createOrder(customer, Delivery.builder().status(DeliveryStatus.READY).build(), orderItem));
 
         try {
-            if(orderList.size() == 0) throw new IllegalStateException();
-            Order order = orderList.get(0);
+            OrderResponseDto orderDto = new OrderResponseDto(order);
 
-            mockMvc.perform(
-                    get("/order/detail/")
-                            .param("id", String.valueOf(order.getId())))
-                    .andExpect(status().isOk())
-                    .andExpect(view().name("detail"))
-                    .andExpect(model().attribute("order", new OrderResponseDto(order)));
+            ResultActions resultActions = mockMvc.perform(get("/order/detail/" + orderDto.getId()));
+            resultActions.andExpect(status().isOk())
+                    .andExpect(view().name("order/detail"))
+                    .andExpect(model().attributeExists("order"))
+                    .andDo(print());
+            MvcResult mvcResult = resultActions.andReturn();
+            Map<String, Object> modelMap = mvcResult.getModelAndView().getModel();
+            OrderResponseDto model = (OrderResponseDto) modelMap.get("order");
+            assertThat(model.getOrderItems().get(0).getName()).isEqualTo(orderDto.getOrderItems().get(0).getName());
+            assertThat(model.getOrderDate()).isEqualTo(orderDto.getOrderDate());
 
-        }catch (Exception ignored){}
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void listview() throws Exception{
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/order/list"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("list"));
+                .andExpect(view().name("order/list"))
+                .andDo(print());
     }
 
     @Test
     public void listapi(){
         int page = 1;
         int size = 5;
-        try {
-            Customer entity = customerRepository.findAll().stream()
-                    .filter(c -> c.getOrderList().size() > 9)
-                    .findFirst()
-                    .orElseThrow(IllegalStateException::new);
-            CustomerResponseDto customer = new CustomerResponseDto(entity); // for session
+        Customer customer = customerRepository.save(Customer.builder()
+                    .name("test customer")
+                    .email("test email")
+                    .password("test pw")
+                    .build());
+        CustomerResponseDto customerDto = new CustomerResponseDto(customer);
 
-            Page<Order> orderList = orderRepository.findAllByCustomer_Id(customer.getId(), PageRequest.of(page, size));
-            ItemService.ListByPaging<OrderResponseDto> body =
+        Seller seller = sellerRepository.save(Seller.builder()
+                .name("test seller")
+                .email("test email")
+                .password("test pw")
+                .build()
+        );
+        Item item = itemRepository.save(Item.createItem(Category.ANIMAL_GOODS, seller, "test item", 10000, 100));
+
+        List<Order> orders = new ArrayList<>();
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), 1);
+        Order order = Order.createOrder(customer, Delivery.builder().status(DeliveryStatus.READY).build(), orderItem);
+        for(int i=0; i<10; i++) {
+            orders.add(order);
+        }
+        orderRepository.saveAll(orders);
+
+        Page<Order> orderList = orderRepository.findAllByCustomer_Id(customer.getId(), PageRequest.of(0, size));
+        ItemService.ListByPaging<OrderResponseDto> body =
                     new ItemService.ListByPaging<>(
                             orderList.getTotalPages(),
                             orderList.getContent().stream()
@@ -105,17 +158,23 @@ public class OrderControllerIntegrationTest {
                                 .collect(Collectors.toList())
                     );
 
+
+        try{
             MultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
             requestParam.add("page", String.valueOf(page));
             requestParam.add("size", String.valueOf(size));
-
+            ObjectMapper objectMapper = new ObjectMapper();
+            String result = objectMapper.writeValueAsString(body);
             mockMvc.perform(
-                    get("/list/api")
-                            .sessionAttr("customer", customer)
+                    get("/order/list/api")
+                            .sessionAttr("customer", customerDto)
                             .params(requestParam))
                     .andExpect(status().isOk())
-                    .andExpect(content().json(String.valueOf(body)));
+                    .andExpect(content().json(result))
+                    .andDo(print());
 
-        }catch (Exception ignored){}
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
