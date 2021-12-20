809ff57a7709d3bd686d515c1a028ff5cbb83d71
jhw127 <jhw127@naver.com>
2021-12-21 00:11:19 +0900

    [scripts/stop.sh] ps pid 반환 방식 변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,10 +4,12 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh # import
 
+IDLE_PROFILE=$(find_idle_profile)
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+IDLE_PID=$(ps -ef | grep java | grep ${IDLE_PROFILE} | awk '{print $2}')
+# $(lsof -ti tcp:${IDLE_PORT})
 echo "> IDLE_PID = $IDLE_PID"
 
 if [ -z ${IDLE_PID} ]
